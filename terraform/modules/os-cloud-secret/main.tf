ddad0ed9b0555054024c749e4706e1a752d08c2f
iverly <github@iverly.net>
2024-04-17 16:38:46 +0200

    feat(terraform): add os-cloud-secret module
    Signed-off-by: iverly <github@iverly.net>

---
+++ terraform/modules/os-cloud-secret/main.tf
@@ -0,0 +1,33 @@
+resource "openstack_identity_application_credential_v3" "this" {
+  name  = var.name
+  roles = ["reader", "member", "load-balancer_member"]
+}
+
+locals {
+  cloud_config = <<EOF
+[Global]
+auth-url = ${var.openstack_auth_url}
+application-credential-id = ${openstack_identity_application_credential_v3.this.id}
+application-credential-secret = ${openstack_identity_application_credential_v3.this.secret}
+region = ${openstack_identity_application_credential_v3.this.region}
+tls-insecure = true
+
+[LoadBalancer]
+use-octavia=true
+floating-network-id=${var.network_external_id}
+subnet-id=${var.network_internal_subnet_id}
+EOF
+}
+
+resource "kubernetes_secret_v1" "this" {
+  metadata {
+    name      = "os-cloud-credentials"
+    namespace = "kube-system"
+  }
+
+  data = {
+    "cloud.conf" = local.cloud_config
+  }
+
+  depends_on = [openstack_identity_application_credential_v3.this]
+}
---
+++ terraform/modules/os-cloud-secret/variables.tf
@@ -0,0 +1,20 @@
+variable "name" {
+  description = "The name of the application credential"
+}
+
+variable "cluster_config" {
+  description = "The raw Kubernetes cluster configuration"
+  type        = string
+}
+
+variable "openstack_auth_url" {
+  description = "URL of the OpenStack authentication endpoint"
+}
+
+variable "network_external_id" {
+  description = "ID of the external network"
+}
+
+variable "network_internal_subnet_id" {
+  description = "ID of the internal subnet"
+}
