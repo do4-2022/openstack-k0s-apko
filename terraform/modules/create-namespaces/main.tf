ff70e56feefb36f135fa6a9153615f08ca1a80c1
iverly <github@iverly.net>
2024-04-18 09:21:51 +0200

    feat(terraform): create cert-manager and nginx namespaces
    Signed-off-by: iverly <github@iverly.net>

--- terraform/modules/create-namespaces/main.tf
+++ terraform/modules/create-namespaces/main.tf
@@ -3,3 +3,15 @@ resource "kubernetes_namespace_v1" "flux-system" {
     name = "flux-system"
   }
 }
+
+resource "kubernetes_namespace_v1" "cert-manager" {
+  metadata {
+    name = "cert-manager"
+  }
+}
+
+resource "kubernetes_namespace_v1" "nginx" {
+  metadata {
+    name = "nginx"
+  }
+}
