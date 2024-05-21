aeca58138de29db41135c579a0f93e07bd6606dd
iverly <github@iverly.net>
2024-05-21 10:44:14 +0200

    feat(app): change world to do
    Signed-off-by: iverly <github@iverly.net>

--- apps/hello-world/main.go
+++ apps/hello-world/main.go
@@ -10,7 +10,7 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello World!")
+		c.String(http.StatusOK, "Hello DO!")
 	})
 
 	r.Run() // listen on 0.0.0.0:8080
